e50a43aeb2d1ccab3087924fe8f45bbb4fd42b56
0xKyPre <k.preining1@students.htl-leonding.ac.at>
2026-04-13 08:09:25 +0200

    created template project for download

---
+++ WMCTemplate/app/src/main/java/at/ac/htl_leonding/wmc_test/wmc_template/MainActivity.kt
@@ -0,0 +1,47 @@
+package at.ac.htl_leonding.wmc_test.wmc_template
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import at.ac.htl_leonding.wmc_test.wmc_template.ui.theme.WMCTemplateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WMCTemplateTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WMCTemplateTheme {
+        Greeting("Android")
+    }
+}
